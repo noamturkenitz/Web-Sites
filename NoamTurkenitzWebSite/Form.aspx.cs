9d0d6b90493a048d10002621274292f7bf3d8ea1
OriZarmany <orizar16@gmail.com>
2025-11-11 16:45:14 +0200

    master start and end

--- NoamTurkenitzWebSite/Form.aspx.cs
+++ NoamTurkenitzWebSite/Form.aspx.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public partial class noam_turkenitz_tochna_MasterPage2 : System.Web.UI.MasterPage
+public partial class Form : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
